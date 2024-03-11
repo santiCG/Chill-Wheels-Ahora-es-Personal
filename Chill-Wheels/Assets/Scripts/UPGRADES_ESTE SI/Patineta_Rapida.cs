d6315fa72e549eda4bb1f701300b4cf5667ac58c
IssaMs46 <isams2004.1@gmail.com>
2024-03-11 09:19:22 -0500

    Lista patineta Rápida

--- Chill-Wheels/Assets/Scripts/UPGRADES_ESTE SI/Patineta_Rapida.cs
+++ Chill-Wheels/Assets/Scripts/UPGRADES_ESTE SI/Patineta_Rapida.cs
@@ -1,11 +1,23 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static Cinemachine.DocumentationSortingAttribute;
 
 public class Patineta_Rapida : MonoBehaviour
 {
+    [SerializeField] private AmountPizzas amountPizzas;
+
+    public PlayerController playerController;
+    public float nuevaVelocidad;
+    public float duracionCambioVelocidad = 5f;
+    public float velocidadOriginal;
+
+
+
+
     public GameObject descripcion;
-    //public GameObject Boton_normal;
+    private float costo = 345f;
+
     void Start()
     {
         descripcion.SetActive(false);
@@ -25,8 +37,36 @@ public class Patineta_Rapida : MonoBehaviour
 
     public void onClick()
     {
+        if (amountPizzas.Pizzas >= costo)
+        {
+            
+            if (playerController != null)
+            {
+                //playerController.speed = nuevaVelocidad;
+                //Debug.Log("velocidad modificada a:" + nuevaVelocidad);
+                StartCoroutine(CambiarVelocidadTemporal());
+            }
+        }
+
+        else 
+        {
+            Debug.Log("No tiene suficientes pizzas, necesarias: " + costo);
+        }
+
+       
         Debug.Log("aqui va la accion de click");
 
 
+    }
+
+    IEnumerator CambiarVelocidadTemporal()
+    {
+        velocidadOriginal = playerController.speed; // Guardar la velocidad original
+
+        playerController.speed = nuevaVelocidad; // Cambiar a la nueva velocidad
+
+        yield return new WaitForSeconds(duracionCambioVelocidad); // Esperar el tiempo especificado
+
+        playerController.speed = velocidadOriginal; // Restaurar la velocidad original
     }
 }
