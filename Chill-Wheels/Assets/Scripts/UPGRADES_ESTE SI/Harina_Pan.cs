7425e5e69acc93bf7fed45457ba22898f4e930af
IssaMs46 <isams2004.1@gmail.com>
2024-03-11 10:36:12 -0500

    lista Harina Pan

--- Chill-Wheels/Assets/Scripts/UPGRADES_ESTE SI/Harina_Pan.cs
+++ Chill-Wheels/Assets/Scripts/UPGRADES_ESTE SI/Harina_Pan.cs
@@ -4,8 +4,18 @@ using UnityEngine;
 
 public class Harina_Pan : MonoBehaviour
 {
+    [SerializeField] private AmountPizzas amountPizzas;
+    [SerializeField] private Piz_x_seg pizzasPorSegundo;
+    public PlayerController playerController;
+
+    private float multiplicadorInicial = 2f; // Multiplicador inicial para la cantidad de pizzas por segundo
+    private float duracionMultiplicador = 5f; // Duración del multiplicador en segundos
+
+
     public GameObject descripcion;
-    //public GameObject Boton_normal;
+
+    private float costo = 5000f;
+
     void Start()
     {
         descripcion.SetActive(false);
@@ -25,8 +35,39 @@ public class Harina_Pan : MonoBehaviour
 
     public void onClick()
     {
-        Debug.Log("aqui va la accion de click");
+        if (amountPizzas.Pizzas >= costo)
+        {
+
+            if (playerController != null)
+            {
+                if (amountPizzas != null && pizzasPorSegundo != null)
+                {
+                    // Obtener la cantidad de pizzas por segundo
+                    float pizzasPorSegundoValue = pizzasPorSegundo.pizzas_seg;
+
+                    //aqui lo quiero multiplicar por dos, despues de un minuto, que vuelva a la normalidad
+
+                    StartCoroutine(MultiplicarPizzasPorSegundo(pizzasPorSegundoValue));
 
 
+                }
+            }
+        }
+
+        else
+        {
+            Debug.Log("No tiene suficientes pizzas, necesarias: " + costo);
+        }
+
+
+    }
+
+    IEnumerator MultiplicarPizzasPorSegundo(float valorInicial)
+    {
+        pizzasPorSegundo.pizzas_seg *= multiplicadorInicial; // Multiplicar por el valor inicial
+
+        yield return new WaitForSeconds(duracionMultiplicador); // Esperar el tiempo especificado
+
+        pizzasPorSegundo.pizzas_seg = valorInicial; // Volver al valor inicial
     }
 }
