e13913634075a2da25823e0cdfa6ac1abb612499
IssaMs46 <isams2004.1@gmail.com>
2024-03-11 08:46:57 -0500

    Medio arreglo de upgrades

---
+++ Chill-Wheels/Assets/Scripts/UPGRADES_ESTE SI/Harina_Pan.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Harina_Pan : MonoBehaviour
+{
+    public GameObject descripcion;
+    //public GameObject Boton_normal;
+    void Start()
+    {
+        descripcion.SetActive(false);
+
+    }
+    public void OnMouseOver()
+    {
+        descripcion.SetActive(true);
+        Debug.Log("Deteccion mouse");
+    }
+
+    public void OnMouseExit()
+    {
+        descripcion.SetActive(false);
+        Debug.Log("salida mouse");
+    }
+
+    public void onClick()
+    {
+        Debug.Log("aqui va la accion de click");
+
+
+    }
+}
