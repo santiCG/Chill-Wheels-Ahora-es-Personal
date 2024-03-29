09ec1d6937d54c8d497f41e39760de375ef86616
IssaMs46 <isams2004.1@gmail.com>
2024-03-28 19:45:28 -0500

    avance en mejoras, faltan algunas de pag 2, todas pag 3 y upgrades

--- Chill-Wheels/Assets/Scripts/Upgrades/Rodilleras.cs
+++ Chill-Wheels/Assets/Scripts/Upgrades/Rodilleras.cs
@@ -1,37 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class Rodilleras : MonoBehaviour
 {
     [SerializeField] private Piz_x_seg pizzas_x_seg;
     [SerializeField] private AmountPizzas amountPizzas;
 
+    [SerializeField] private TextMeshProUGUI costoText; // Usamos TextMeshProUGUI en lugar de Text
+    [SerializeField] private TextMeshProUGUI nivelText; // Nuevo TextMeshProUGUI para el nivel
+    [SerializeField] private GameObject descripcionContainer; // Nuevo GameObject para el contenedor de la descripción
+    [SerializeField] private TextMeshProUGUI descripcionText; // Nuevo TextMeshProUGUI para la descripción
+    [SerializeField] private Image backgroundImage;
+
     // Produccion del horno
     private float produccion = 50f;
 
     //Costo
     private int nivel = 1;
-    private float costo = 1150f;
+    public float costo = 1150f;
 
-    public GameObject descripcion;
-    //public GameObject Boton_normal;
+
     void Start()
     {
-        descripcion.SetActive(false);
+        descripcionContainer.SetActive(false); // Desactiva el contenedor de la descripción al inicio
+        ActualizarTextoCosto();
+        ActualizarTextoNivel();
+        ActualizarTextoDescripcion();
+        ActualizarColorFondo();
 
+    }
+    void Update()
+    {
+        ActualizarColorFondo(); // Llama a la función de actualización de color de fondo en cada fotograma
     }
 
     // Update is called once per frame
     public void OnMouseOver()
     {
-        descripcion.SetActive(true);
+        descripcionContainer.SetActive(true);
+        descripcionText.gameObject.SetActive(true);
         Debug.Log("Deteccion mouse");
     }
 
     public void OnMouseExit()
     {
-        descripcion.SetActive(false);
+        descripcionContainer.SetActive(false);
+        descripcionText.gameObject.SetActive(false);
         Debug.Log("salida mouse");
     }
 
@@ -41,17 +58,54 @@ public class Rodilleras : MonoBehaviour
         {
             pizzas_x_seg.AumentarPiz_Seg(produccion);
 
-            produccion += Mathf.Sqrt(produccion);
+            produccion += Mathf.Round(Mathf.Sqrt(produccion));
 
             amountPizzas.Pizzas -= costo;
 
             costo = Mathf.Round(1000 * Mathf.Pow(1.15f, nivel));
 
             nivel++;
+
+            ActualizarTextoCosto();
+            ActualizarTextoNivel();
+            ActualizarTextoDescripcion();
+            ActualizarColorFondo();
         }
         else
         {
             Debug.Log("No te alcanza");
+            backgroundImage.color = Color.red;
+        }
+
+    }
+
+    private void ActualizarTextoCosto()
+    {
+        costoText.text = "Costo: " + costo.ToString() + " P"; // Actualiza el texto con el nuevo costo
+    }
+
+    private void ActualizarTextoNivel()
+    {
+        nivelText.text = nivel.ToString(); // Actualiza el texto con el nuevo nivel
+    }
+
+    private void ActualizarTextoDescripcion()
+    {
+        descripcionText.text = "porque cada paso cuenta, ¡y tus rodillas merecen un paseo suave como una nube!\nProducción: + " + produccion.ToString() + "p/s"; // Actualiza el texto de la descripción
+    }
+
+    private void ActualizarColorFondo()
+    {
+        // Cambia el color de fondo de la imagen según la cantidad de pizzas
+        if (amountPizzas.Pizzas >= costo)
+        {
+            // Cuando hay suficientes pizzas, el color de fondo vuelve a su estado original
+            backgroundImage.color = Color.white; // Por ejemplo, cambia el color a blanco
+        }
+        else
+        {
+            // Cuando no hay suficientes pizzas, el color de fondo se establece en rojo
+            backgroundImage.color = Color.red; // Por ejemplo, cambia el color a rojo
         }
     }
 }
--- Chill-Wheels/Assets/Scripts/Upgrades/Tennis.cs
+++ Chill-Wheels/Assets/Scripts/Upgrades/Tennis.cs
@@ -1,37 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Tennis : MonoBehaviour
 {
     [SerializeField] private Piz_x_seg pizzas_x_seg;
     [SerializeField] private AmountPizzas amountPizzas;
 
+    //prueba
+    [SerializeField] private TextMeshProUGUI costoText; // Usamos TextMeshProUGUI en lugar de Text
+    [SerializeField] private TextMeshProUGUI nivelText; // Nuevo TextMeshProUGUI para el nivel
+    [SerializeField] private GameObject descripcionContainer; // Nuevo GameObject para el contenedor de la descripción
+    [SerializeField] private TextMeshProUGUI descripcionText; // Nuevo TextMeshProUGUI para la descripción
+    [SerializeField] private Image backgroundImage; // Referencia al componente Image de la imagen de fondo
+
+
     // Produccion del horno
     private float produccion = 5f;
 
     //Costo
     private int nivel = 1;
-    private float costo = 115f;
+    public float costo = 115f;
 
-    public GameObject descripcion;
-    //public GameObject Boton_normal;
+
     void Start()
     {
-        descripcion.SetActive(false);
+        descripcionContainer.SetActive(false); // Desactiva el contenedor de la descripción al inicio
+        ActualizarTextoCosto();
+        ActualizarTextoNivel();
+        ActualizarTextoDescripcion();
+        ActualizarColorFondo(); // Actualiza el color de fondo al inicio
 
+    }
+    void Update()
+    {
+        ActualizarColorFondo(); // Llama a la función de actualización de color de fondo en cada fotograma
     }
 
     // Update is called once per frame
     public void OnMouseOver()
     {
-        descripcion.SetActive(true);
+        descripcionContainer.SetActive(true);
+        descripcionText.gameObject.SetActive(true);
         Debug.Log("Deteccion mouse");
     }
 
     public void OnMouseExit()
     {
-        descripcion.SetActive(false);
+        descripcionContainer.SetActive(false);
+        descripcionText.gameObject.SetActive(false);
         Debug.Log("salida mouse");
     }
 
@@ -41,17 +60,58 @@ public class Tennis : MonoBehaviour
         {
             pizzas_x_seg.AumentarPiz_Seg(produccion);
 
-            produccion += Mathf.Sqrt(produccion);
+            produccion += Mathf.Round(Mathf.Sqrt(produccion));
 
             amountPizzas.Pizzas -= costo;
 
             costo = Mathf.Round(260 * Mathf.Pow(1.15f, nivel));
 
             nivel++;
+
+            // Actualiza el texto del costo después de comprar
+            ActualizarTextoCosto();
+            ActualizarTextoNivel();
+            ActualizarTextoDescripcion();
+            ActualizarColorFondo();
+
         }
         else
         {
             Debug.Log("No te alcanza");
+            backgroundImage.color = Color.red;
+        }
+
+
+
+    }
+
+    private void ActualizarTextoCosto()
+    {
+        costoText.text = "Costo: " + costo.ToString() + " P"; // Actualiza el texto con el nuevo costo
+    }
+
+    private void ActualizarTextoNivel()
+    {
+        nivelText.text = nivel.ToString(); // Actualiza el texto con el nuevo nivel
+    }
+
+    private void ActualizarTextoDescripcion()
+    {
+        descripcionText.text = "Ten estilo y mejora tu agarre!\nProducción: + " + produccion.ToString() + "p/s"; // Actualiza el texto de la descripción
+    }
+
+    private void ActualizarColorFondo()
+    {
+        // Cambia el color de fondo de la imagen según la cantidad de pizzas
+        if (amountPizzas.Pizzas >= costo)
+        {
+            // Cuando hay suficientes pizzas, el color de fondo vuelve a su estado original
+            backgroundImage.color = Color.white; // Por ejemplo, cambia el color a blanco
+        }
+        else
+        {
+            // Cuando no hay suficientes pizzas, el color de fondo se establece en rojo
+            backgroundImage.color = Color.red; // Por ejemplo, cambia el color a rojo
         }
     }
 }
