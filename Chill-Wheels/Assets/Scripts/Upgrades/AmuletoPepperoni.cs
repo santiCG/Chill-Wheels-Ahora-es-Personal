b690cdaa2c911cc3c7265a90ad3ae0075f9d309e
IssaMs46 <isams2004.1@gmail.com>
2024-03-28 23:21:06 -0500

    listas las mejoras, faltan upgrades

--- Chill-Wheels/Assets/Scripts/Upgrades/AmuletoPepperoni.cs
+++ Chill-Wheels/Assets/Scripts/Upgrades/AmuletoPepperoni.cs
@@ -1,37 +1,56 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class AmuletoPepperoni : MonoBehaviour
 {
     [SerializeField] private Piz_x_seg pizzas_x_seg;
     [SerializeField] private AmountPizzas amountPizzas;
 
-    // Produccion del horno
+    //prueba
+    [SerializeField] private TextMeshProUGUI costoText; // Usamos TextMeshProUGUI en lugar de Text
+    [SerializeField] private TextMeshProUGUI nivelText; // Nuevo TextMeshProUGUI para el nivel
+    [SerializeField] private GameObject descripcionContainer; // Nuevo GameObject para el contenedor de la descripción
+    [SerializeField] private TextMeshProUGUI descripcionText; // Nuevo TextMeshProUGUI para la descripción
+    [SerializeField] private Image backgroundImage;
+
+    // Produccion 
     private float produccion = 1000f;
 
     //Costo
     private int nivel = 1;
-    private float costo = 57500f;
+    public float costo = 57500f;
 
-    public GameObject descripcion;
-    //public GameObject Boton_normal;
+ 
     void Start()
     {
-        descripcion.SetActive(false);
+        descripcionContainer.SetActive(false); // Desactiva el contenedor de la descripción al inicio
+        ActualizarTextoCosto();
+        ActualizarTextoNivel();
+        ActualizarTextoDescripcion();
+        ActualizarColorFondo();
 
+    }
+
+    void Update()
+    {
+        ActualizarColorFondo(); // Llama a la función de actualización de color de fondo en cada fotograma
     }
 
     // Update is called once per frame
     public void OnMouseOver()
     {
-        descripcion.SetActive(true);
+        descripcionContainer.SetActive(true);
+        descripcionText.gameObject.SetActive(true);
         Debug.Log("Deteccion mouse");
     }
 
     public void OnMouseExit()
     {
-        descripcion.SetActive(false);
+        descripcionContainer.SetActive(false);
+        descripcionText.gameObject.SetActive(false);
         Debug.Log("salida mouse");
     }
 
@@ -41,17 +60,54 @@ public class AmuletoPepperoni : MonoBehaviour
         {
             pizzas_x_seg.AumentarPiz_Seg(produccion);
 
-            produccion += Mathf.Sqrt(produccion);
+            produccion += Mathf.Round(Mathf.Sqrt(produccion));
 
             amountPizzas.Pizzas -= costo;
 
             costo = Mathf.Round(50000 * Mathf.Pow(1.15f, nivel));
 
             nivel++;
+
+            ActualizarTextoCosto();
+            ActualizarTextoNivel();
+            ActualizarTextoDescripcion();
+            ActualizarColorFondo();
         }
         else
         {
             Debug.Log("No te alcanza");
+            backgroundImage.color = Color.red;
+        }
+    }
+
+    private void ActualizarTextoCosto()
+    {
+        costoText.text = "Costo: " + costo.ToString() + " P"; // Actualiza el texto con el nuevo costo
+
+    }
+
+    private void ActualizarTextoNivel()
+    {
+        nivelText.text = nivel.ToString(); // Actualiza el texto con el nuevo nivel
+    }
+
+    private void ActualizarTextoDescripcion()
+    {
+        descripcionText.text = "Que la suerte esté siempre de tu lado!\nProducción: + " + produccion.ToString() + "p/s"; // Actualiza el texto de la descripción
+    }
+
+    private void ActualizarColorFondo()
+    {
+        // Cambia el color de fondo de la imagen según la cantidad de pizzas
+        if (amountPizzas.Pizzas >= costo)
+        {
+            // Cuando hay suficientes pizzas, el color de fondo vuelve a su estado original
+            backgroundImage.color = Color.white; // Por ejemplo, cambia el color a blanco
+        }
+        else
+        {
+            // Cuando no hay suficientes pizzas, el color de fondo se establece en rojo
+            backgroundImage.color = Color.red; // Por ejemplo, cambia el color a rojo
         }
     }
 }
